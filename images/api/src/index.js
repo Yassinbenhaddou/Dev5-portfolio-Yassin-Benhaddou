335e9ccca6d07783c1d651788ada1327e7db1a84
Yassinbenhaddou <43605983+Yassinbenhaddou@users.noreply.github.com>
2022-12-01 11:41:38 +0100

    feat: add comments and creat routes

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 
 server.use(express.json());
 
+
 // connect to the postgres database 
 const pg = require('knex')({
 
@@ -17,46 +18,53 @@ const pg = require('knex')({
 });
 
 
+
+
+
 /** 
  * when the server starts, console log the port number and start the table initialisation
  */
-server.listen(PORT, () => {
+ server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`); 
     initialiseTables(); // initialise the tables
 });
 
 
 
-
-
-
-/**
- * initializations of the database tables 
- * addition of the planets table
+/**template retunrSpaceShip:
+ *  id: string - id of the spaceship
+ *  name: string - name of the spaceship
+ *  fictional: boolean - is the spaceship fictional
+ *  img: string - url to the image of the spaceship
+ * 
+ * @api {get} /spaceships Get all spaceships
+ * @returns retunrSpaceShip
  */
-
-
-
 server.get("/ships", async (req, res) => {
-
-
-
-
     const ships = await pg.select().from("spaceShips");
     res.json(ships);
 
     console.log("get ships");
 
-
-
 });
 
-
-
+/** 
+ * template retunrSpaceShip:
+ *  id: string - id of the spaceship
+ *  name: string - name of the spaceship
+ *  fictional: boolean - is the spaceship fictional
+ *  img: string - url to the image of the spaceship
+ * 
+ * @api {post} /spaceships Add a spaceship
+ * @param {String} name Name of the spaceship
+ * @param {Boolean} fictional Is the spaceship fictional?
+ * @param {String} img URL of the spaceship image
+ * @returns retunrSpaceShip - The spaceship that was added
+ */
 server.post("/PostShips", async (req, res) => {
     console.log("post ships");
     const {
-        name,
+        name, 
         motor,
         wings,
         reactor,
@@ -85,6 +93,11 @@ server.post("/PostShips", async (req, res) => {
 });
 
 
+
+/**
+ * delete a ship endpoint 
+ * @param {string} id - the id of the ship to be deleted
+ */
 server.delete("/DeleteShips/:id", async (req, res) => {
     console.log("delete ships");
     const id = req.params.id;
@@ -96,9 +109,22 @@ server.delete("/DeleteShips/:id", async (req, res) => {
 });
 
 
-/** 
+
+/**
+ * template retunrSpaceShip:
+ *  id: string - id of the spaceship
+ *  name: string - name of the spaceship
+ *  motor: string - name motor of the spaceship
+ *  wings: string - name wings of the spaceship
+ *  reactor: string - name reactor of the spaceship
+ *  shield: string - name shield of the spaceship
+ *  weapon: string - name weapon of the spaceship
+ *  pilot: string - name pilot of the spaceship
  * 
- */
+ * @api {put} /spaceships/:id Update a spaceship
+ * @param {String} id ID of the spaceship
+ * @return retunrSpaceShip
+*/
 server.put("/PutShips/:id", async (req, res) => {
     console.log("put ships");
     const id = req.params.id;
@@ -175,3 +201,5 @@ async function initialiseTables() {
  * Yassin 
  * 
  */
+
+
