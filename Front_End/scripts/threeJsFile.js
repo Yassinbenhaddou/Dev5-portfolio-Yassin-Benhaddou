156a7281f7025a1f87c26dcaa1ce3793eb32e8bc
Yassin Benhaddou <yassine.bachiri13@gmail.com>
2022-12-18 00:59:19 +0100

    space ship maker done ! without use of server

--- Front_End/scripts/threeJsFile.js
+++ Front_End/scripts/threeJsFile.js
@@ -48,7 +48,7 @@ export const threeJsApplication = {
 
 
         cube.position.x = 0;
-        cube.position.y = -0, 5;
+        cube.position.y = 0;
         cube.position.z = 0;
 
         sphere.position.x = 0;
@@ -105,14 +105,188 @@ export const threeJsApplication = {
             document.getElementById("rightWingColor").value
         ];
 
+
+
+        this.get3DspaceShipFromServer(spaceShipName, frontHead, body, backPart, leftWing, rightWing);
+
+        // generate the 3d space ship
+
+        // send the space ship form info to the server
+
+    },
+    get3DspaceShipFromServer: function (spaceShipName, frontHead, body, backPart, leftWing, rightWing) {
+
+        // send the space ship form info to the server
+
+        // get the 3d space ship from the server
+
+        // generate the 3d space ship
+        this.generate3DspaceShip(spaceShipName, frontHead, body, backPart, leftWing, rightWing);
+
+    },
+    generate3DspaceShip: function (spaceShipName, frontHead, body, backPart, leftWing, rightWing) {
         console.log(spaceShipName);
         console.log(frontHead);
         console.log(body);
         console.log(backPart);
         console.log(leftWing);
         console.log(rightWing);
+
+        // create a new Three.js scene
+        const scene = new THREE.Scene();
+
+        // set the background color of the scene
+        scene.background = new THREE.Color(0x03030);
+
+        // add a camera perspective to the scene
+        const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 500);
+
+        // Create a new Three.js renderer
+        const renderer = new THREE.WebGLRenderer({
+            canvas: document.querySelector("#generatedSpaceShipCanva")
+        });
+
+        // Set the size of the renderer
+        renderer.setSize(window.innerWidth / 4, window.innerHeight / 4);
+
+        // Create a new Three.js cube
+        const cubeGeometryMesh = new THREE.CubeGeometry(0.5, 0.5, 0.5);
+        const sphereGeometryMesh = new THREE.SphereGeometry(0.3, 0.3, 0.3);
+        const coneGeometryMesh = new THREE.ConeGeometry(0.5, 0.5, 0.5);
+        const cylinderGeometryMesh = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 0.5);
+
+        //remove the # from the color code and convert it to hex
+        const frontHeadColor = parseInt(frontHead[1].replace("#", "0x"));
+        const bodyColor = parseInt(body[1].replace("#", "0x"));
+        const backPartColor = parseInt(backPart[1].replace("#", "0x"));
+        const leftWingColor = parseInt(leftWing[1].replace("#", "0x"));
+        const rightWingColor = parseInt(rightWing[1].replace("#", "0x"));
+
+
+        const frontHeadMaterial = new THREE.MeshBasicMaterial({
+            color: frontHeadColor
+        });
+
+        const bodyMaterial = new THREE.MeshBasicMaterial({
+            color: bodyColor
+        });
+
+        const backPartMaterial = new THREE.MeshBasicMaterial({
+            color: backPartColor
+        });
+
+        const leftWingMaterial = new THREE.MeshBasicMaterial({
+            color: leftWingColor
+        });
+
+        const rightWingMaterial = new THREE.MeshBasicMaterial({
+            color: rightWingColor
+        });
+
+        var shipFrontHead = new THREE.Mesh(coneGeometryMesh, frontHeadMaterial);
+
+        var shipBody = new THREE.Mesh(cubeGeometryMesh, backPartMaterial);
+
+        var shipBackPart = new THREE.Mesh(sphereGeometryMesh, bodyMaterial);
+
+        var shipLeftWing = new THREE.Mesh(cubeGeometryMesh, leftWingMaterial);
+
+        var shipRightWing = new THREE.Mesh(cubeGeometryMesh, rightWingMaterial);
+
+        if(frontHead[0] == "cone"){
+            shipFrontHead = new THREE.Mesh(coneGeometryMesh, frontHeadMaterial);
+        }else if(frontHead[0] == "cube"){
+            shipFrontHead = new THREE.Mesh(cubeGeometryMesh, frontHeadMaterial);
+        }else if(frontHead[0] == "sphere"){
+            shipFrontHead = new THREE.Mesh(sphereGeometryMesh, frontHeadMaterial);
+        }
+
+        if(body[0] == "cone"){
+            shipBody = new THREE.Mesh(coneGeometryMesh, bodyMaterial);
+        }else if(body[0] == "cube"){
+            shipBody = new THREE.Mesh(cubeGeometryMesh, bodyMaterial);
+        }else if(body[0] == "sphere"){
+            shipBody = new THREE.Mesh(sphereGeometryMesh, bodyMaterial);
+        }
+
+        if(backPart[0] == "cone"){
+            shipBackPart = new THREE.Mesh(coneGeometryMesh, backPartMaterial);
+        }else if(backPart[0] == "cube"){
+            shipBackPart = new THREE.Mesh(cubeGeometryMesh, backPartMaterial);
+        }else if(backPart[0] == "sphere"){
+            shipBackPart = new THREE.Mesh(sphereGeometryMesh, backPartMaterial);
+        }else if(backPart[0] == "cylinder"){
+            shipBackPart = new THREE.Mesh(cylinderGeometryMesh, backPartMaterial);
+        }
+
+        if(leftWing[0] == "cone"){
+            shipLeftWing = new THREE.Mesh(coneGeometryMesh, leftWingMaterial);
+        }else if(leftWing[0] == "cube"){
+            shipLeftWing = new THREE.Mesh(cubeGeometryMesh, leftWingMaterial);
+        }else if(leftWing[0] == "sphere"){
+            shipLeftWing = new THREE.Mesh(sphereGeometryMesh, leftWingMaterial);
+        }
+
+        if(rightWing[0] == "cone"){
+            shipRightWing = new THREE.Mesh(coneGeometryMesh, rightWingMaterial);
+        }else if(rightWing[0] == "cube"){
+            shipRightWing = new THREE.Mesh(cubeGeometryMesh, rightWingMaterial);
+        }else if(rightWing[0] == "sphere"){
+            shipRightWing = new THREE.Mesh(sphereGeometryMesh, rightWingMaterial);
+        }
+
         
-        // send the space ship form info to the server
+
+
+        const geometryGroup = new THREE.Group();
+
+        geometryGroup.add(shipFrontHead);
+        geometryGroup.add(shipBody);
+        geometryGroup.add(shipBackPart);
+        geometryGroup.add(shipLeftWing);
+        geometryGroup.add(shipRightWing);
+
+        shipFrontHead.position.x = 0;
+        shipFrontHead.position.y = 0.5;
+        shipFrontHead.position.z = 0;
+
+        shipBody.position.x = 0;
+        shipBody.position.y = 0;
+        shipBody.position.z = 0;
+
+        shipBackPart.position.x = 0;
+        shipBackPart.position.y = -0.5;
+        shipBackPart.position.z = 0;
+
+        shipLeftWing.position.x = -0.5;
+        shipLeftWing.position.y = 0;
+        shipLeftWing.position.z = 0;
+
+        shipRightWing.position.x = 0.5;
+        shipRightWing.position.y = 0;
+        shipRightWing.position.z = 0;
+
+        
+
+        // Add the cube to the scene
+        scene.add(geometryGroup);
+
+        // Set the position of the camera
+        camera.position.z = 5;
+
+        // Start the renderer
+        renderer.render(scene, camera);
+
+        // Animate the scene (rotate the cube)
+        function animate() {
+            requestAnimationFrame(animate); //call the animate function again when the browser is ready to render the next frame
+            // geometryGroup.rotation.x += 0.005; //rotate the cube on the x axis (you can change the value to make it rotate faster or slower)
+            geometryGroup.rotation.y += 0.005; //rotate the cube on the y axis (you can change the value to make it rotate faster or slower)
+            geometryGroup.rotation.z += 0.005; //rotate the cube on the z axis (you can change the value to make it rotate faster or slower)
+            geometryGroup.rotation.x += 0.005; //rotate the cube on the z axis (you can change the value to make it rotate faster or slower)
+            renderer.render(scene, camera);
+        }
+        animate(); 
 
     }
 
